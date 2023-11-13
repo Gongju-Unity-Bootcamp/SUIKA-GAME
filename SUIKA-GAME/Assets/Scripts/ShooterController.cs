b60007396470e648684e8ec622c71245ef2b9dcb
JungYeonSuk <a01075106199@gmail.com>
2023-11-13 19:57:57 +0900

    게임 슈터 로직변경

--- SUIKA-GAME/Assets/Scripts/ShooterController.cs
+++ SUIKA-GAME/Assets/Scripts/ShooterController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +8,8 @@ public class ShooterController : MonoBehaviour
     [SerializeField] private GameObject planet;
     [SerializeField] private Transform spawnPoint;
 
-    [SerializeField] private float tiltAngle = 30f;
-    [SerializeField] private float angleRange = 0.5f;
+    [SerializeField] private float shooterSpeed = 30f;
+    [SerializeField] private float shooterRange = 4.4f;
     [SerializeField] private float shootPower = 1500f;
 
     private GameObject stuff;
@@ -20,23 +21,13 @@ public class ShooterController : MonoBehaviour
 
     private void Update()
     {
-        OnKeyPress();
+        OnMouseInput();
     }
 
-    private void OnKeyPress()
+    private void OnMouseInput()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            OnShoot();
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            OnLeftRotate();
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            OnRightRotate();
-        }
+        float horizontal = Input.GetAxis("Mouse X");
+        transform.Translate(new Vector3(horizontal * Time.deltaTime * shooterSpeed, 0, 0));
     }
 
     private void OnShoot()
@@ -44,21 +35,5 @@ public class ShooterController : MonoBehaviour
         GameObject _planet = Instantiate(planet, spawnPoint.position, spawnPoint.rotation);
         _planet.GetComponent<Rigidbody2D>().AddForce(_planet.transform.up * shootPower);
         _planet.transform.parent = stuff.transform;
-    }
-
-    private void OnLeftRotate()
-    {
-        if (transform.rotation.z < angleRange)
-        {
-            transform.Rotate(new Vector3(0, 0, tiltAngle * Time.deltaTime));
-        }
-    }
-
-    private void OnRightRotate()
-    {
-        if (transform.rotation.z > -angleRange)
-        {
-            transform.Rotate(new Vector3(0, 0, -tiltAngle * Time.deltaTime));
-        }
     }
 }
