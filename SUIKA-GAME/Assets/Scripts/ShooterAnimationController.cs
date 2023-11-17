fa64cb41b63cf4713e67a82db7460669cc3f8484
김세현 <t990421@naver.com>
2023-11-17 11:09:29 +0900

    Add Shooter Animation Logic

---
+++ SUIKA-GAME/Assets/Scripts/ShooterAnimationController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using UnityEngine;
+
+public class ShooterAnimationController : MonoBehaviour
+{
+    [SerializeField] private GameObject shooter;
+    [SerializeField] private float animationDelay = 2f;
+    [SerializeField] private float shooterInitDelay = 0.75f;
+
+    private float fixShooterYValue = 0.09f;
+    private Vector3 value = Vector3.zero;
+    private bool isLanded;
+
+    private void Start()
+    {
+        OnShooterAnimationInit(true);
+    }
+
+    private void OnShooterAnimationInit(bool _isActive)
+    {
+        shooter.SetActive(!_isActive);
+        gameObject.SetActive(_isActive);
+        // SoundManager.Play.PlayEffect("ShooterTakeoff");
+    }
+
+    private void Update()
+    {
+        OnAnimationTranslate();
+    }
+
+    private void OnAnimationTranslate()
+    {
+        if (!isLanded && transform.position == shooter.transform.position)
+        {
+            // SoundManager.Play.PlayEffect("ShooterLand");
+            isLanded = true;
+            StartCoroutine(OnShooterInitDelay(false));
+        }
+
+        if (transform.position.y <= shooter.transform.position.y + fixShooterYValue)
+        {
+            transform.position = shooter.transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation, shooter.transform.rotation, animationDelay * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.SmoothDamp(gameObject.transform.position, shooter.transform.position, ref value, animationDelay);
+        }
+    }
+
+    private IEnumerator OnShooterInitDelay(bool _isActive)
+    {
+        yield return new WaitForSeconds(shooterInitDelay);
+
+        shooter.SetActive(!_isActive);
+        gameObject.SetActive(_isActive);
+    }
+}
